1ed660bc469282863c4b507bfc349fceba00023d
Coder-the-noob <moinmatubbar6@gmail.com>
2025-09-28 12:46:53 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Countires from './Components/Countries/Countires'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <Countires></Countires>
+      
+      
+    </>
+  )
+}
+
+export default App
