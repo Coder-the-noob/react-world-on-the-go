3e3b22c4056e3db6e8fa4e331ac6360dabed5e9e
Coder-the-noob <moinmatubbar6@gmail.com>
2025-09-30 20:46:46 +0600

    how to add conditional css

--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Country.css';
 
 const Country = ({country}) => {
+    const [visited, setVisited] = useState(false);
     // console.log(country.flags.flags.png);
 
     const handleVisited = () => {
-        console.log("Button Clicked");
+        setVisited(!visited);
     }
     return (
-        <div className='country'>
+        <div className={`country ${visited && 'country-visited'}`}>
             <img src={country?.flags?.flags?.png} alt={country.flags.flags.alt} />
             <h3>Name: {country.name.common} </h3>
             <p>Population: {country.population.population}</p>
             <p>Area: {country.area.area} {
                 country.area.area > 300000 ? "Big Country" : "Small Country"}</p>
-            <button onClick={handleVisited}>Not Visited</button> </div>
+            <button onClick={handleVisited}>
+                { visited ? 'visited':'Not Visited'}</button> </div>
     );
 };
 
