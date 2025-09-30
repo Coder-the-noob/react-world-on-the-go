c63c7e3007481029350a54fc182bce08daca52d9
Coder-the-noob <moinmatubbar6@gmail.com>
2025-09-30 20:30:53 +0600

    Added the externa css file and conditional rendering

--- src/Components/Countries/Countires.jsx
+++ src/Components/Countries/Countires.jsx
@@ -1,21 +1,21 @@
-import React, { use } from 'react';
-import Country from '../Country/Country';
+import React, { use } from "react";
+import Country from "../Country/Country";
+import "./Countries.css";
 
-const Countires = ({coutriesPromise}) => {
-
-    const countriesData = use(coutriesPromise);
-    const countries = countriesData.countries;
-    // console.log(countries);
-    return (
-        <div>
-            <h1>In the Countries:{countries.length} </h1>
-            {
-                countries.map(country => <Country 
-                    key={country.cca3.cca3}
-                    country = {country}></Country>)
-            }
-        </div>
-    );
+const Countires = ({ coutriesPromise }) => {
+  const countriesData = use(coutriesPromise);
+  const countries = countriesData.countries;
+  // console.log(countries);
+  return (
+    <div>
+      <h1>In the Countries:{countries.length} </h1>
+      <div className="countries">
+        {countries.map((country) => (
+          <Country key={country.cca3.cca3} country={country}></Country>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Countires;
--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import './Country.css';
 
 const Country = ({country}) => {
-    console.log(country.flags.flags.png);
+    // console.log(country.flags.flags.png);
+
+    const handleVisited = () => {
+        console.log("Button Clicked");
+    }
     return (
-        <div>
+        <div className='country'>
             <img src={country?.flags?.flags?.png} alt={country.flags.flags.alt} />
             <h3>Name: {country.name.common} </h3>
             <p>Population: {country.population.population}</p>
-        </div>
+            <p>Area: {country.area.area} {
+                country.area.area > 300000 ? "Big Country" : "Small Country"}</p>
+            <button onClick={handleVisited}>Not Visited</button> </div>
     );
 };
 
